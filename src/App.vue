fb3fe6d185e01ea16bd75cba9cde51d893d72619
liuhui <liuhui@mavericksmobile.com>
2024-09-24 17:19:36 +0800

    feat: auto import route

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
 
 <template>
   <h1 class="w100">1379 地球流浪记</h1>
+  <router-view></router-view>
 </template>
 <style scoped lang="less"></style>
